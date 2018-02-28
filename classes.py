ad47878116dea8383c6d3af949b516ff6dc356d0
medyas <medyassinesabri@gmail.com>
2018-02-28 12:48:04 +0100

    updated dashboard UI

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ import MySQLdb, datetime
 
 class dataBase:
     def __init__(self):
-        self.db = MySQLdb.connect("localhost","rfid","password","userData" )
+        self.db = MySQLdb.connect("localhost","python","python3","userData" )
         self.cursor = self.db.cursor()
 
     def checkUser(self, username):
@@ -12,9 +12,125 @@ class dataBase:
             self.cursor.execute(sql)
             data = self.cursor.fetchall()
         except:
-            return False, None
+            return False, ""
 
         if(len(data) != 0):
             return True, data[0]
         else:
-            return False, None
+            return False, ""
+
+    def empJson(self, d):
+        data = []
+        for l in d:
+            temp = {
+                    'id': l[0],
+                    'firstname':l[1],
+                    'lastname':l[2],
+                    'address':l[3],
+                    'card_uid':l[4],
+                    'img_path':l[5] 
+                }
+            data.append(temp)
+        return data
+
+    def findEmployee(self, string):
+        sql = "select * from employees where firstname like '%"+string+"%' or lastname like '%"+string+"%'"
+        d = None
+        try:
+            self.cursor.execute(sql)
+            d = self.cursor.fetchall()
+        except:
+            return False, ""
+        
+        if(len(d) == 0):
+            return False, ""
+
+        data = self.empJson(d)
+
+        return True, data
+
+    def employee(self, uid):
+        sql = "select * from employees where id="+uid
+        d = None
+        try:
+            self.cursor.execute(sql)
+            d = self.cursor.fetchall()
+        except:
+            return False, ""
+
+        if(len(d) != 0):
+            data = self.empJson(d)
+            return True, data
+        else:
+            return True, ""
+
+    def employeeLogs(self, uid):
+        sql = "select * from employee_logs where user_id="+uid+" ORDER BY date, time asc"
+        d = None
+        try:
+            self.cursor.execute(sql)
+            d = self.cursor.fetchall()
+        except:
+            return False, ""
+
+        if(len(d) != 0):
+            data = []
+            for l in d:
+                temp = {
+                        'id': l[0],
+                        'user_id':l[1],
+                        'date':l[2].strftime("%Y-%m-%d"),
+                        'time':str(l[3])
+                    }
+                data.append(temp)
+            return True, data
+        else:
+            return True, ""
+
+
+    def employeesLogs(self):
+        sql = "select * from employee_logs ORDER BY date, time asc"
+        d = None
+        try:
+            self.cursor.execute(sql)
+            d = self.cursor.fetchall()
+        except:
+            return False, ""
+        
+        sql = "select * from employees"
+        e = None
+        try:
+            self.cursor.execute(sql)
+            e = self.cursor.fetchall()
+        except:
+            return False, ""
+
+        if(len(d) != 0 and len(e) != 0):
+            print(d[0][1])
+            data = []
+            for l in d:
+                temp = {
+                        'scan_id': l[0],
+                        'user_id':l[1],
+                        'name': e[l[1]-1][1]+", "+e[l[1]-1][2],
+                        'card_uid': e[l[1]-1][4],
+                        'date':l[2].strftime("%Y-%m-%d"),
+                        'time':str(l[3])
+                    }
+                data.append(temp)
+            return True, data
+        else:
+            return True, ""
+
+    def addUser(first, last, user, password, email, edit, add, block, delete):
+        sql = "insert into users (firstname, lastname, username, password, email) values (%s,)" % (first, last)
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except:
+            return False, "Couldn't add the requested user!"
+
+        return True, "User has been added"
+
+
+        
--- rfid.py
+++ rfid.py
@@ -1,27 +1,55 @@
 import os, requests, json, classes, datetime
-from flask import Flask, request, session, redirect, render_template, url_for, send_from_directory
+from flask import Flask, request, session, redirect, render_template, url_for, send_from_directory, make_response
+import flask_login
 
-
+# database connection
 db = classes.dataBase()
 
+# app init
 app = Flask(__name__)
 
+# app config
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY=os.urandom(1024),
+    SESSION_REFRESH_EACH_REQUEST=False,
+    PERMANENT_SESSION_LIFETIME=datetime.timedelta(hours=1)
+)
+
+
+#login_manager = flask_login.LoginManager()
+#login_manager.init_app(app)
+
+# find cookie
+def checkCookie():
+    if(request.cookies.get('username')):
+        session['username'] = request.cookies.get('username')
+        session['id'] = request.cookies.get('id')
+        return True
+    else:
+        return False
+
+# index-login page
 @app.route('/')
 def index():
-    if not session:
+    if not session and not checkCookie():
         return render_template('index.html')
     else:
         return redirect(url_for('dashboard'))
 
+# login url
 @app.route('/login/', methods=['POST',  'GET'])
 def login():
     if request.method == 'POST':
-        status, data = db.checkUser(request.form['username'])
+        status, data = db.checkUser(request.form['username'].lower())
         if(status):
             if(request.form['password'] == data[5]):
-                session['username'] = request.form['username']
+                session['username'] = request.form['username'].lower()
                 session['id'] = data[0]
-                return url_for('dashboard')
+                resp = make_response(url_for('dashboard'))
+                resp.set_cookie('username', session['username'])
+                resp.set_cookie('id', str(session['id']))
+                return resp
             else:
                 return "error"
         else:
@@ -29,84 +57,175 @@ def login():
     else:
         return redirect(url_for('index'))
 
+# dashboard page
 @app.route('/dashboard/', methods=['POST',  'GET'])
 def dashboard():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
-        return render_template('header.html') + render_template('dashboard.html') + render_template('footer.html')
+        if(request.method == 'GET'):
+            return render_template('header.html') + render_template('dashboard.html') + render_template('footer.html')
+        else:
+            status, data = db.employeesLogs()
+            if(status):
+                return json.dumps(data)
+            else:
+                return ""
 
+# about page
 @app.route('/about/')
 def about():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
         return render_template('header.html') + render_template('about.html') + render_template('footer.html')
 
+# contact page
 @app.route('/conctact/')
 def contact():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
         return render_template('header.html') + render_template('contact.html') + render_template('footer.html')
 
-@app.route('/settings/')
+@app.route('/settings/', methods=['POST',  'GET'])
 def settings():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
         return render_template('header.html') + render_template('settings.html') + render_template('footer.html')
 
-@app.route('/logout/')
-def logout():
-    if not session:
-        return redirect(url_for('index'))
-    else:
-        session.pop('username', None)
-        return redirect(url_for('index'))
 
-@app.route('/adduser/')
+# add user page
+@app.route('/adduser/', methods=['POST',  'GET'])
 def adduser():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
-        return render_template('header.html') + render_template('adduser.html') + render_template('footer.html')
+        if(request.method == 'GET'):
+            return render_template('header.html') + render_template('adduser.html') + render_template('footer.html')
+        else:
+            status, msg = db.addUser(request.form['firstname'], request.form['lastname'], request.form['username'], request.form['password'], request.form['email']request.form['edit']request.form['add']request.form['block'], request.form['delete'])
+            if(status):
+                data = {
+                        'status': status,
+                        'msg': msg,
+                        'class': 'alert-success'
+                    }
+                return json.dumps(data)
+            else:
+                data = {
+                        'status': status,
+                        'msg': msg,
+                        'class': 'alert-danger'
+                    }
+                return json.dumps(data)
 
-@app.route('/addemployee/')
+# add employee page
+@app.route('/addemployee/', methods=['POST',  'GET'])
 def addemployee():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
-        return render_template('header.html') + render_template('addemployee.html') + render_template('footer.html')
+        if(request.method == 'GET'):
+            return render_template('header.html') + render_template('addemployee.html') + render_template('footer.html')
+        else:
+            status, data = db.employeesLogs()
+            if(status):
+                return json.dumps(data)
+            else:
+                return ""
 
-@app.route('/block/')
+# bloock employee page
+@app.route('/block/', methods=['POST',  'GET'])
 def block():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
-        return render_template('header.html') + render_template('block.html') + render_template('footer.html')
+        if(request.method == 'GET'):
+            return render_template('header.html') + render_template('block.html') + render_template('footer.html')
+        else:
+            status, data = db.employeesLogs()
+            if(status):
+                return json.dumps(data)
+            else:
+                return ""
 
-@app.route('/deleteuser/')
+# delete user page
+@app.route('/deleteuser/', methods=['POST',  'GET'])
 def deleteuser():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
-        return render_template('header.html') + render_template('deleteuser.html') + render_template('footer.html')
+        if(request.method == 'GET'):
+            return render_template('header.html') + render_template('deleteuser.html') + render_template('footer.html')
+        else:
+            status, data = db.employeesLogs()
+            if(status):
+                return json.dumps(data)
+            else:
+                return ""
 
-@app.route('/deleteemployee/')
+# delete employee page
+@app.route('/deleteemployee/', methods=['POST',  'GET'])
 def deleteemployee():
-    if not session:
+    if not session and not checkCookie():
         return redirect(url_for('index'))
     else:
         return render_template('header.html') + render_template('deleteemployee.html') + render_template('footer.html')
 
 
+# logout page
+@app.route('/logout/')
+def logout():
+    if not session and not checkCookie():
+        return redirect(url_for('index'))
+    else:
+        session.pop('username', None)
+        session.pop('id', None)
+        resp = make_response(redirect(url_for('index')))
+        resp.set_cookie('username', '', expires=0)
+        resp.set_cookie('id',  '', expires=0)
+        return resp
+
+# 404 not found page
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect(url_for('index'))
+    return render_template('404.html') 
 
 
-app.secret_key = os.urandom(1024)
+
+# match the string to employee name
+@app.route('/findemployee/<string>')
+def findemployee(string=""):
+    if not session and not checkCookie():
+        return redirect(url_for('index'))
+    else:
+        if(string == ""):
+            return ""
+        status, data = db.findEmployee(string)
+        if(status):
+            return json.dumps(data)
+        else:
+            return ""
+
+# find employee by uid
+@app.route('/employee=<uid>')
+def employee(uid=""):
+    if not session and not checkCookie():
+        return redirect(url_for('index'))
+    else:
+        status, emData = db.employee(uid)
+        if(status and emData!=None):
+            status, logsData = db.employeeLogs(uid)
+            temp = {
+                    'employee':emData,
+                    'logs': logsData
+                }
+            return json.dumps(temp)
+        else:
+            return ''
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
